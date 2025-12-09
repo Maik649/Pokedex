9633563eefb9f6c812e357a3015de419ee2e4ac8
Maik648 <maikpankow@gmail.com>
2025-12-09 22:04:59 +0100

    add pocecard html and variablen

--- scrips/templade.js
+++ scrips/templade.js
@@ -1,1 +1,13 @@
-console.log(`Templade Geht`);
+async function getPocemonCard(poceName, poceImg, poceId, typeName, typeName_2) {
+  return `<div class="card border-primary mb-3" style="max-width: 18rem;">
+                      <p class="card-id">#${poceId}</p>
+                    <div class="card-header">
+                    <img id="cardImgHeder" src="${poceImg}" alt=""></div>
+                    <div class="card-body text-primary">
+                        <h5 class="card-title">${poceName}</h5>
+                        <div class="types-content">
+                        <p class="card-text">${typeName}</p><p class="card-text">${typeName_2}</p>
+                        </div>
+                    </div>
+                </div>`;
+}
