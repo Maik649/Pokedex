8d3900e015faa597f88c3e6c25cec0ea496c4286
Maik648 <maikpankow@gmail.com>
2025-12-20 13:42:40 +0100

    rename function,  and ids, add about card,  add search funtion, add show left ,showRight function

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let BASE_URL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offsets=${offse
 let pocedexCard = [];
 let allPokemmons = [];
 let inputValue = document.getElementById("search");
-let dialogCard = document.getElementById("card-1");
+let dialogCard = document.getElementById("dialog-content");
 
 async function init() {
-  await pokemonData();
+  await pokemonCard();
 }
 
-async function loadPokedex() {
+async function loadPocedex() {
   try {
     const response = await fetch(BASE_URL);
     let poceCard = await response.json();
@@ -20,10 +20,10 @@ async function loadPokedex() {
   }
 }
 
-async function pokemonCard() {
+async function pokomeonData() {
   showLoader();
   try {
-    await loadPokedex(pocedexCard);
+    await loadPocedex(pocedexCard);
     for (let i = 0; i < pocedexCard.length; i++) {
       let response = await fetch(pocedexCard[i].url);
       let singledata = await response.json();
@@ -34,47 +34,64 @@ async function pokemonCard() {
   }
 }
 
-async function pokemonData() {
-  await pokemonCard(allPokemmons);
-  for (const key in allPokemmons) {
-  if (!Object.hasOwn(allPokemmons, key)) continue;
-    let element = allPokemmons[key];
-   document.getElementById("pocemons").innerHTML += getPocemonCard(element);
+async function pokemonCard() {
+  await pokomeonData(allPokemmons);
+  for (let i = 0; i < allPokemmons.length; i++) {
+    let element = allPokemmons[i];
+    let speciesUrl = await fetch(element.species.url);
+    let specie = await speciesUrl.json();
+    let color = specie.color.name;
+    document.getElementById("pocemons").innerHTML += getPokemonCard(
+      element,
+      i,
+      color
+    );
   }
 }
 
 async function cardDialog(i) {
-   await pokemonCard(allPokemmons);
-   let pokemon = allPokemmons[i];
-   let currentIndex = i;
-   document.querySelector("#card-1").innerHTML = getPocemonCardDialog(pokemon, currentIndex);
+  let element = allPokemmons[i];
+  document.querySelector("#card").innerHTML = getPokemonCardDialog(element, i);
+  await aboutCardPocemon(i);
   dialogCard.showModal();
 }
 
-// async function aboutCardPocemon() {
-//       document.getElementById("card-body-content").innerHTML = getPocemonDetailsCardDialog();
-//     }
-// async function searchPocemon(event) {
-//   event.stopPropagation();
-// }
+async function aboutCardPocemon(i) {
+  let element = allPokemmons[i];
+  let id = allPokemmons[i].id;
+  let typeUrl = await fetch(`https://pokeapi.co/api/v2/nature/${id}/`);
+  let type = await typeUrl.json();
+  let specieale = type.pokeathlon_stat_changes[0].pokeathlon_stat.name;
+  document.getElementById("card-body-content").innerHTML = getAboutCardDialog(element,specieale,i);
+}
+
+async function searchPocemon() {
+ 
+  for (let i = 0; i < allPokemmons.length; i++) {
+    let element = allPokemmons[i];
+   if (inputValue.value == element.id || inputValue.value == element.name) {
+     document.getElementById("card").innerHTML = getPokemonCardDialog(element,i);
+     await cardDialog(i);
+     await aboutCardPocemon(i);
+   }
+  }
+}
+async function showLeft(i) {
+  let currentIndex = (i - 1 + allPokemmons.length) % allPokemmons.length;
+  let element = allPokemmons[currentIndex];
+  document.querySelector("#card").innerHTML = getPokemonCardDialog(element,currentIndex);
+}
+
+async function showlRight(i) {
+  let currentIndex = i;
+  currentIndex = (i + 1) % allPokemmons.length;
+  let element = allPokemmons[currentIndex];
+  document.querySelector("#card").innerHTML = getPokemonCardDialog(element,currentIndex);
+}
 
 async function morePocemon() {
-  document.getElementById("pocemons").innerHTML="";
+  document.getElementById("pocemons").innerHTML = "";
   offset = offset += 20;
   BASE_URL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-  await pokemonData();
-}
-
-async function showLeft(currentIndex) {
-  currentIndex = (currentIndex - 1 + allPokemmons.length) % allPokemmons.length;
-  currentIndex = allPokemmons[currentIndex];
-  currentIndex--;
-  document.querySelector("#card-1").innerHTML = getPocemonCardDialog(currentIndex)
- await cardDialog();
-}
-
-function showlRight(currentIndex) {
-  currentIndex = (currentIndex + 1 + allPokemmons.length) % allPokemmons.length;
-allPokemmons[currentIndex];
-  document.querySelector("#card-1").innerHTML = getPocemonCardDialog();
+  await pokemonCard();
 }
