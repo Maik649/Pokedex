b22e8db28891c9d396d445b2bd7585317083d3a8
Maik648 <maikpankow@gmail.com>
2025-12-22 20:19:51 +0100

    cleaning code from logs, add try catch

--- script.js
+++ script.js
@@ -40,6 +40,9 @@ async function pokemonCard() {
   await pokomeonData(allPokemmons);
   for (let i = 0; i < allPokemmons.length; i++) {
     let pokemon = allPokemmons[i];
+
+    //console.log(pokemon.species.url);
+    
     if (pokemon.types.length == 1) {
       pokemon.types[0].type.name.charAt(0).toUpperCase() +pokemon.types[0].type.name.slice(1);
     } else {
@@ -70,27 +73,48 @@ async function baseStatCardPocemon(i) {
 
 async function shinyCardPocemon(i) {
   let pokemon = allPokemmons[i];
+  try {
+    
     let speciURL = await fetch(pokemon.species.url);
     let pokemon2 = await speciURL.json();
+    console.log(pokemon2.evolution_chain.url);
     document.getElementById("card-body-content").innerHTML = getShinyCardDialog(pokemon, pokemon2);
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
 
-// async function genderCardPocemon(i) {
-//   let pokemon = allPokemmons[i];
-//   let evolutionURL = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokemon.id}/`);
-//   let pokemon3 = await evolutionURL.json();
-//  console.log(pokemon3.chain.evolves_to[0].evolves_to[0].species.name);
+async function genderCardPocemon(i) {
+  let pokemon = allPokemmons[i];
+  try {
+    let evolutionURL = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokemon.id}/`);
+  let pokemon3 = await evolutionURL.json();
+    console.log(pokemon3.chain.evolves_to[0]);
  
-//   document.getElementById("card-body-content").innerHTML = getGenderCardDialog(pokemon, pokemon3)
-// }
+  document.getElementById("card-body-content").innerHTML = getGenderCardDialog(pokemon, pokemon3)
+  } catch (error) {
+    console.log(error);
+    
+  }
+  
+}
 
 async function searchPocemon() {
   for (let i = 0; i < allPokemmons.length; i++) {
     let pokemon = allPokemmons[i];
+  
+    if (inputValue.value.trim() == "") {
+       inputValue.value = "Bitte etwas Eintrag";
+    } else {
       if (inputValue.value == pokemon.id || inputValue.value == pokemon.name) {
-        document.getElementById("card").innerHTML = getPokemonCardDialog(pokemon,i);
+        document.getElementById("card").innerHTML = getPokemonCardDialog(
+          pokemon,
+          i
+        );
         await cardDialog(i);
         await aboutCardPocemon(i);
+      }
     }
   }
 }
