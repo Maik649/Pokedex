fa34c6ad3c27cb242f34e5a41316e4dc834824b9
Maik648 <maikpankow@gmail.com>
2025-12-23 20:43:30 +0100

    add new  query  for button

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let allPokemmons = [];
 let inputValue = document.getElementById("search");
 let dialogCard = document.getElementById("dialog-content");
 
-
 async function init() {
   await pokemonCard();
 }
@@ -41,21 +40,16 @@ async function pokemonCard() {
   await pokomeonData(allPokemmons);
   for (let i = 0; i < allPokemmons.length; i++) {
     let pokemon = allPokemmons[i];
-   //let node = document.getElementById("icon-content");
-   //node.querySelectorAll("type-btn").classList.add("icon-content-remove");
-   if (pokemon.types.length == 1) {
-        pokemon.types[0].type.name.charAt(0).toUpperCase() +pokemon.types[0].type.name.slice(1);
-        // if (node.parentNode) {
-        //       node.parentNode.removeChild(node);
-        // }
+    let button2 = `<button id="btn-icon-button" class="type-btn"><img id="cardImgHeder" class="type-img" src="./image/${pokemon.types.length == 1 ? "" : pokemon.types[1].type.name}.png" alt="${pokemon.name}"></button>`
+    if (pokemon.types.length == 1) {
+      pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1);
     } else {
-      pokemon.types[0].type.name.charAt(0).toUpperCase() +pokemon.types[0].type.name.slice(1);
-      pokemon.types[1].type.name.charAt(0).toUpperCase() +pokemon.types[1].type.name.slice(1);
+      pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1);
+      button2;
+      }
+      document.getElementById("pocemons").innerHTML += getPokemonCard(pokemon, i,button2);
     }
-    console.log(pokemon);
-    document.getElementById("pocemons").innerHTML += getPokemonCard(pokemon, i);
   }
-}
 
 async function cardDialog(i) {
   let pokemon = allPokemmons[i];
@@ -77,14 +71,12 @@ async function baseStatCardPocemon(i) {
 async function shinyCardPocemon(i) {
   let pokemon = allPokemmons[i];
   try {
-    
     let speciURL = await fetch(pokemon.species.url);
     let pokemon2 = await speciURL.json();
     console.log(pokemon2.evolution_chain.url);
     document.getElementById("card-body-content").innerHTML = getShinyCardDialog(pokemon, pokemon2);
   } catch (error) {
     console.log(error);
-    
   }
 }
 
@@ -92,29 +84,22 @@ async function genderCardPocemon(i) {
   let pokemon = allPokemmons[i];
   try {
     let evolutionURL = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokemon.id}/`);
-  let pokemon3 = await evolutionURL.json();
-    console.log(pokemon3.chain.evolves_to[0]);
- 
-  document.getElementById("card-body-content").innerHTML = getGenderCardDialog(pokemon, pokemon3)
+    let pokemon3 = await evolutionURL.json();
+    //console.log(pokemon3.chain.evolves_to[0]);
+    document.getElementById("card-body-content").innerHTML = getGenderCardDialog(pokemon, pokemon3)
   } catch (error) {
     console.log(error);
-    
   }
-  
 }
 
 async function searchPocemon() {
   for (let i = 0; i < allPokemmons.length; i++) {
     let pokemon = allPokemmons[i];
-  
     if (inputValue.value.trim() == "") {
        inputValue.value = "Bitte etwas Eintrag";
     } else {
       if (inputValue.value == pokemon.id || inputValue.value == pokemon.name) {
-        document.getElementById("card").innerHTML = getPokemonCardDialog(
-          pokemon,
-          i
-        );
+        document.getElementById("card").innerHTML = getPokemonCardDialog(pokemon,i);
         await cardDialog(i);
         await aboutCardPocemon(i);
       }
@@ -127,7 +112,6 @@ async function showLeft(i) {
   let pokemon = allPokemmons[currentIndex];
   document.querySelector("#card").innerHTML = getPokemonCardDialog(pokemon,currentIndex);
   await aboutCardPocemon(i);
-
 }
 
 async function showlRight(i) {
